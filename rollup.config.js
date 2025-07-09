b354db628fe541a59590dbdb46b55be99b4ac00f
Egor Prokhorov <befealmellow@gmail.com>
2025-07-09 09:47:14 +0400

    инициализация

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import { babel } from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import { terser } from "rollup-plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+import pkg from "./package.json";
+import babelConfig from "./babel.config.json";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+let paths = pkg.exports["."];
+
+const config = {
+  external: ["effector", /effector\-vue/],
+  input: "src/index.ts",
+  output: [
+    {
+      file: paths.require,
+      format: "cjs",
+      sourcemap: false,
+    },
+    {
+      file: paths.import,
+      format: "es",
+      sourcemap: false,
+    },
+  ],
+  plugins: [
+    typescript({ tsconfig: "./tsconfig.json" }),
+    replace({
+      preventAssignment: true,
+    }),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      extensions,
+      ...babelConfig,
+    }),
+    nodeResolve({ extensions }),
+    commonjs({ extensions }),
+    terser(),
+  ],
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+export { createForm } from "./effector-forms/factory";
+export { useForm, makeInputBinder } from "./vue-hooks";
+
+export {
+  AnyFormValues,
+  ExternalFormUnits,
+  ExternalFieldUnits,
+  FormConfig,
+  FieldConfig,
+  Rule,
+  RuleResolver,
+  ValidationError,
+  Validator,
+  ValidationEvent,
+  ValidationResult,
+  FormValues,
+  Form,
+  FieldShape,
+  FormLike,
+  StringKey,
+} from "./types";
